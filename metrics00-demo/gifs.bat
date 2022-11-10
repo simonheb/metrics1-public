12f48406da51472841fe10047ce267cddeab4679
Simon Heß <simon.h.hess@gmail.com>
2022-11-10 15:20:54 +0100

    adding demo slides

---
+++ metrics00-demo/gifs.bat
@@ -0,0 +1,10 @@
+magick convert -delay 50 -loop 0 -density 200 demo.pdf[3-36] samplingdist.gif
+magick convert -delay 50 -loop 0 -density 200 demo.pdf[38-70] heterosced.gif
+magick convert -delay 50 -loop 0 -density 200 demo.pdf[72-93] measurement_error.gif
+magick convert -delay 60 -loop 0 -density 200 demo.pdf[95-112] truncation.gif
+magick convert -delay 60 -loop 0 -density 200 demo.pdf[114-149] wlln.gif
+magick convert -delay 60 -loop 0 -density 200 demo.pdf[151-183] clt.gif
+<<<<<<< HEAD
+magick convert -delay 60 -loop 0 -density 200 demo.pdf[185-199,199] cis.gif
+=======
+>>>>>>> 3c820588c7ab016d21f01b7cc67848f05c5820ab
